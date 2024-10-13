fe8495dae420f77aa855b171b63b03c07eec3625
Comma Device <device@comma.ai>
2024-10-13 14:49:10 -0300

    change query_fw_versions.py to extract_keys.py

--- hello_text_window.py
+++ hello_text_window.py
@@ -3,9 +3,8 @@ from common.text_window import TextWindow
 import subprocess
 from openpilot.common.basedir import BASEDIR
 import os
-from openpilot.system.manager.process_config import managed_processes
 
 if __name__ == '__main__':
   os.system("pkill -f pandad")
-  with TextWindow(subprocess.Popen("./justprintsomething.py --debug", shell=True, cwd=os.path.join(BASEDIR), stdout=subprocess.PIPE).stdout.read()) as t:
+  with TextWindow(subprocess.Popen("./extract_key.py", shell=True, cwd=os.path.join(BASEDIR), stdout=subprocess.PIPE).stdout.read()) as t:
     t.wait_for_exit()
